0bc78ce3a2395fd09f7a0f6b034a1af68d1dcd98
James Billingham <git@jamesbillingham.com>
2016-12-27 12:04:51 +0000

    Player's hands set-up, and handle two-player games

--- game/index.js
+++ game/index.js
@@ -1,13 +1,51 @@
 import createDeck from './create-deck';
 
+const minPlayers = 2;
+const maxPlayers = 4;
+const cardsToBurnNormal = 1;
+const cardsToBurnExtra = 3;
+
 export default class Game {
-	drawPile = [];
-	discardPile = [];
-	burnedCard = null;
+	drawDeck = [];
+	playersHands = [];
+	discardedCards = [];
+	burnedCards = [];
+
+	constructor(playerCount) {
+		if (!Number.isInteger(playerCount))
+			throw new Error('invalid parameter playerCount - not an integer');
+
+		if (playerCount < minPlayers)
+			throw new Error(`invalid parameter playerCount - minimum ${minPlayers} players`);
+
+		if (playerCount > maxPlayers)
+			throw new Error(`invalid parameter playerCount - maximum ${maxPlayers} players`);
+
+		this.playersHands = new Array(playerCount);
+		this.resetCards();
+	}
+
+	get playerCount() {
+		return this.playersHands.length;
+	}
 
 	resetCards() {
-		this.drawPile = createDeck();
-		this.discardPile = [];
-		this.burnedCard = this.drawPile.pop();
+		this.drawDeck = createDeck();
+		this.discardedCards = [];
+		this.burnedCards = [];
+
+		for (let i = 0; i < this.playerCount; i++)
+			this.playersHands[i] = [];
+
+		let cardsToBurn = cardsToBurnNormal;
+
+		if (this.playerCount === minPlayers)
+			cardsToBurn += cardsToBurnExtra;
+
+		for (let i = 0; i < cardsToBurn; i++)
+			this.burnedCards.push(this.drawDeck.pop());
+
+		for (let i = 0; i < this.playerCount; i++)
+			this.playersHands[i] = [this.drawDeck.pop()];
 	}
 }
